10fe1aeb6273415c831ec2e3adf73643792b23f0
rizkydwiananto <rizkydwiananto@gmail.com>
2021-06-26 19:10:25 +0700

    Belajar Next.JS Basic

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"; // Digunakan untuk memanggil effect dari javascript contoh setTimeout
+import { useRouter } from 'next/router';
+
+export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 2000);
+  }, []);
+  return (
+    <div>
+      <h1 className="title-not-found">Ooops.....</h1>
+      <h1 className="title-not-found">Halaman yang anda cari tidak ditemukan.</h1>
+    </div>
+  );
+}
